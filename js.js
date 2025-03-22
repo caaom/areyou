9d618813025446a875fe48560596d05bd4699e46
Femboy <skibidi@furaffinity.org>
2025-03-22 11:30:16 +0000

    Improves UI and CSS, fixes JS variable initialization
    Adds rainbow animation to text in CSS
    Fixes incorrect increment operator in JS
    Enhances HTML structure and IDs
    Includes new CSS files for different button actions

--- js.js
+++ js.js
@@ -1,5 +1,6 @@
+var c = 0;
 function no() {
-	c =+ 1;
+	c += 1;
 	if (c == 1) {
 		document.getElementById("main").innerHTML = "that was a misclick RIGHT";
 	}
@@ -12,9 +13,21 @@ function no() {
 	if (c == 4) {
 		document.getElementById("main").innerHTML = "FINE YOU ABUSIVE BOYFREND";
 		document.getElementById("q_container").remove();
+		let link = document.createElement('link');
+		link.rel = "stylesheet";
+		link.type = "text/css";
+		link.href = "badboy.css";
+		document.getElementsByTagName('HEAD')[0].appendChild(link);
+		document.getElementById("suck").remove();
 	}
 }
 function yes() {
 	document.getElementById("main").innerHTML = "good boy";
 	document.getElementById("q_container").remove();
+	let link = document.createElement('link');
+	link.rel = "stylesheet";
+	link.type = "text/css";
+	link.href = "goodboy.css";
+	document.getElementsByTagName('HEAD')[0].appendChild(link);
+	document.getElementById("suck").remove();
 }
